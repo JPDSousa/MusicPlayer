61120f18016fbccb93c710e1a419f1e2ee4d8eb9
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-09 09:00:36 +0100

    updated search

--- src/app/musicplayer/util/Search.java
+++ src/app/musicplayer/util/Search.java
@@ -1,130 +1,56 @@
 package app.musicplayer.util;
 
-import app.musicplayer.model.Library;
-import app.musicplayer.model.Song;
-import app.musicplayer.model.Album;
-import app.musicplayer.model.Artist;
 import app.musicplayer.model.SearchResult;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Pattern;
+
+import org.rookit.mongodb.DBManager;
+
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
-public class Search {
+@SuppressWarnings("javadoc")
+public class Search implements Runnable{
+	
+	private final BooleanProperty hasResults;
+	private SearchResult result;
+	private Thread searchThread;
+	private final DBManager database;
+	private String text;
 
-    private static BooleanProperty hasResults = new SimpleBooleanProperty(false);
-    private static SearchResult result;
-    private static Thread searchThread;
+	public Search(DBManager database) {
+		this.database = database;
+		hasResults = new SimpleBooleanProperty(false);
+	}
 
-    public static BooleanProperty hasResultsProperty() { return hasResults; }
+	public BooleanProperty hasResultsProperty() { 
+		return hasResults; 
+	}
 
-    public static SearchResult getResult() {
-        hasResults.set(false);
-        return result;
-    }
+	public SearchResult getResult() {
+		hasResults.set(false);
+		return result;
+	}
 
-    public static void search(String searchText) {
-        if (searchThread != null && searchThread.isAlive()) {
-            searchThread.interrupt();
-        }
+	public synchronized void search(String searchText) {
+		if (searchThread != null && searchThread.isAlive()) {
+			searchThread.interrupt();
+		}
+		text = searchText.toUpperCase();
+		searchThread = new Thread(this);
+		searchThread.start();
+	}
 
-        String text = searchText.toUpperCase();
-
-        searchThread = new Thread(() -> {
-            try {
-
-                hasResults.set(false);
-
-                List<Song> songResults = Library.getSongs().stream()
-                        .filter(song -> song.getTitle().toUpperCase().contains(text))
-                        .sorted((x, y) -> {
-                            boolean xMatch = x.getTitle().toUpperCase().equals(text);
-                            boolean yMatch = y.getTitle().toUpperCase().equals(text);
-                            if (xMatch && yMatch) return 0;
-                            if (xMatch) return -1;
-                            if (yMatch) return 1;
-
-                            boolean xStartWith = x.getTitle().toUpperCase().startsWith(text);
-                            boolean yStartWith = y.getTitle().toUpperCase().startsWith(text);
-                            if (xStartWith && yStartWith) return 0;
-                            if (xStartWith) return -1;
-                            if (yStartWith) return 1;
-
-                            boolean xContains = x.getTitle().toUpperCase().contains(" " + text);
-                            boolean yContains = y.getTitle().toUpperCase().contains(" " + text);
-                            if (xContains && yContains) return 0;
-                            if (xContains) return -1;
-                            if (yContains) return 1;
-                            return 0;
-                        })
-                        .collect(Collectors.toList());
-
-                if (searchThread.isInterrupted()) { throw new InterruptedException(); }
-
-                List<Album> albumResults = Library.getAlbums().stream()
-                        .filter(album -> album.getTitle().toUpperCase().contains(text))
-                        .sorted((x, y) -> {
-                            boolean xEqual = x.getTitle().toUpperCase().equals(text);
-                            boolean yEqual = y.getTitle().toUpperCase().equals(text);
-                            if (xEqual && yEqual) return 0;
-                            if (xEqual) return -1;
-                            if (yEqual) return 1;
-
-                            boolean xStartWith = x.getTitle().toUpperCase().startsWith(text);
-                            boolean yStartWith = y.getTitle().toUpperCase().startsWith(text);
-                            if (xStartWith && yStartWith) return 0;
-                            if (xStartWith) return -1;
-                            if (yStartWith) return 1;
-
-                            boolean xContains = x.getTitle().toUpperCase().contains(" " + text);
-                            boolean yContains = y.getTitle().toUpperCase().contains(" " + text);
-                            if (xContains && yContains) return 0;
-                            if (xContains) return -1;
-                            if (yContains) return 1;
-                            return 0;
-                        })
-                        .collect(Collectors.toList());
-
-                if (searchThread.isInterrupted()) { throw new InterruptedException(); }
-
-                List<Artist> artistResults = Library.getArtists().stream()
-                        .filter(artist -> artist.getTitle().toUpperCase().contains(text))
-                        .sorted((x, y) -> {
-                            boolean xMatch = x.getTitle().toUpperCase().equals(text);
-                            boolean yMatch = y.getTitle().toUpperCase().equals(text);
-                            if (xMatch && yMatch) return 0;
-                            if (xMatch) return -1;
-                            if (yMatch) return 1;
-
-                            boolean xStartWith = x.getTitle().toUpperCase().startsWith(text);
-                            boolean yStartWith = y.getTitle().toUpperCase().startsWith(text);
-                            if (xStartWith && yStartWith) return 0;
-                            if (xStartWith) return -1;
-                            if (yStartWith) return 1;
-
-                            boolean xContains = x.getTitle().toUpperCase().contains(" " + text);
-                            boolean yContains = y.getTitle().toUpperCase().contains(" " + text);
-                            if (xContains && yContains) return 0;
-                            if (xContains) return -1;
-                            if (yContains) return 1;
-                            return 0;
-                        })
-                        .collect(Collectors.toList());
-
-                if (searchThread.isInterrupted()) { throw new InterruptedException(); }
-
-                if (songResults.size() > 3) songResults = songResults.subList(0, 3);
-                if (albumResults.size() > 3) albumResults = albumResults.subList(0, 3);
-                if (artistResults.size() > 3) artistResults = artistResults.subList(0, 3);
-                result = new SearchResult(songResults, albumResults, artistResults);
-
-                hasResults.set(true);
-
-            } catch (InterruptedException ex) {
-                // terminate thread
-            }
-        });
-        searchThread.start();
-    }
+	@Override
+	public void run() {
+		hasResults.set(false);
+		final Pattern regex = Pattern.compile(".*"+Pattern.quote(text), Pattern.CASE_INSENSITIVE);
+		final SearchResult result = new SearchResult();
+		database.getTracks().withTitle(regex).stream().limit(3).forEach(result::addResult);
+		database.getAlbums().withTitle(regex).stream().limit(3).forEach(result::addResult);
+		database.getArtists().withName(regex).stream().limit(3).forEach(result::addResult);
+		database.getGenres().withName(regex).stream().limit(3).forEach(result::addResult);
+		this.result = result;
+		hasResults.set(true);
+	}
 }
